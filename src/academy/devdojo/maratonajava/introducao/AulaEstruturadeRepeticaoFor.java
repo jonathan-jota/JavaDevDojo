c52980855d1af62d05555afa4b76476b6c7ace51
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-05 18:55:48 -0300

    Estrutura de Repetição For

---
+++ src/academy/devdojo/maratonajava/introducao/AulaEstruturadeRepeticaoFor.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class AulaEstruturadeRepeticaoFor {
+    public static void main(String[] args) {
+        //Imprima todos os números pares de 0 até 1000000
+        for (int i = 0;i <= 1000000;  i++) {
+            if (i % 2 ==0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
