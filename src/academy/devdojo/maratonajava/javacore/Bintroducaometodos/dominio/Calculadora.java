5756f95c69c5be78dc549f5244081f92881a4ad1
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-11 20:45:43 -0300

    Métodos Parte 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -4,4 +4,12 @@ public class Calculadora {
     public void somaDoisNumeros() {
         System.out.println(10 + 10);
     }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(25 - 3);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
 }
