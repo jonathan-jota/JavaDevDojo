c3514fb511da45ab50fb64bc6229f60cb5d27480
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-11 20:59:53 -0300

    Métodos Parte 03 - Retorno pt 01

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,11 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0){
+            return 0;
+        }
+        return num1 / num2;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double result = calculadora.divideDoisNumeros(10, 2);
+        System.out.println(result);
+    }
+}
