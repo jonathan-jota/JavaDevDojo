14342b404764a127bb0b243c0b581f54f07f4409
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-10 23:05:48 -0300

    Métodos Parte 01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+}
