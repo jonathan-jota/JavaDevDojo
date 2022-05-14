92b3f176035e827401dfc4f0e8426580d9621ac1
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-14 00:23:07 -0300

    Métodos Parte 05 - Parâmetro tipo Referência pt2

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        estudante.nome = "Jota";
+        estudante.idade = 34;
+        estudante.sexo = 'M';
+
+        estudante.imprime();
+    }
+}
