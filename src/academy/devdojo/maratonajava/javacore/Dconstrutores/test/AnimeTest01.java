0341ec337085765c9176b0e7dbfbb98bcbbcc553
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-17 22:58:15 -0300

    Construtores e Sobrecarga de Construtores

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Jiraya", "Anime", 450, "Ação");
+        Anime anime2 = new Anime();
+
+        anime.imprime();
+        anime2.imprime();
+    }
+}
