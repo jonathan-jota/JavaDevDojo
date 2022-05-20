cbed585f91b81afcb7b3d0311e76f0f41bbcaf48
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-19 21:47:10 -0300

    Métodos Estáticos

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite = 250;
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
@@ -16,6 +16,13 @@ public class Carro {
         System.out.println("Velocidade Máxima " + this.velocidadeMaxima);
         System.out.println("Velocidade Limite " + Carro.velocidadeLimite);
     }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+
+    public static double getVelocidadeLimite() {
+        return velocidadeLimite;    }
 
     public String getNome() {
         return nome;
@@ -33,7 +40,7 @@ public class Carro {
         this.velocidadeMaxima = velocidadeMaxima;
     }
 
-    public double getVelocidadeLimite() {
+    //public double getVelocidadeLimite() {
         return velocidadeLimite;
     }
 
