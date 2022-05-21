a0a15f22a96e1a969543c2f2a624cb437b56ed9b
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-20 23:51:04 -0300

    Associaçao unidirecional um para muitos

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Vini Jr");
+        Time time = new Time("Real Madrid");
+
+        jogador1.setTime(time);
+
+        jogador1.imprime();
+    }
+}
