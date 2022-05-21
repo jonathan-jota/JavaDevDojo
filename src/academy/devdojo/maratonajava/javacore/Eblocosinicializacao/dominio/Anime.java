1e14e674c9ca219af8d60e67443a455cff14dfb7
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-05-20 23:12:13 -0300

    Associaçao - Array com Objetos

--- src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -1,38 +1,29 @@
-package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
 
 public class Anime {
     private String nome;
-    private static int[] episodios;
+    private int[] episodios;
 
-    /**
-     * 0 - O Bloco de Inicialização é executado quando a JVM carregar a classe
-     * 1 - Alocado espaço em memória pro objeto
+    /**1 - Alocado espaço em memória pro objeto
      * 2 - Cada atributo de classe é criado e inicializado com valores default ou por nós definido
      * 3 - BLoco de incialização é executado
      * 4 - Construtor é executado
      */
-    static {
-        System.out.println("Dentro do bloco de inicialização 1");
+    {
+        System.out.println("Dentro do bloco de incialização");
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i + 1;
         }
     }
 
-    static {
-        System.out.println("Dentro do bloco de inicialização estático 2");
-    }
-
-    {
-        System.out.println("Dentro do bloco de inicialização não estático");
-    }
     public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
-        for (int episodio : Anime.episodios) {
-            System.out.print(episodio + " ");
+        for (int episodio: this.episodios) {
+            System.out.println(episodio + " ");
         }
         System.out.println();
     }
@@ -51,7 +42,7 @@ public class Anime {
     }
 
     public void setEpisodios(int[] episodios) {
-        Anime.episodios = episodios;
+        this.episodios = episodios;
     }
 }
 
--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -1,29 +1,38 @@
-package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.domain;
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios;
+    private static int[] episodios;
 
-    /**1 - Alocado espaço em memória pro objeto
+    /**
+     * 0 - O Bloco de Inicialização é executado quando a JVM carregar a classe
+     * 1 - Alocado espaço em memória pro objeto
      * 2 - Cada atributo de classe é criado e inicializado com valores default ou por nós definido
      * 3 - BLoco de incialização é executado
      * 4 - Construtor é executado
      */
-    {
-        System.out.println("Dentro do bloco de incialização");
+    static {
+        System.out.println("Dentro do bloco de inicialização 1");
         episodios = new int[100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i + 1;
         }
     }
 
+    static {
+        System.out.println("Dentro do bloco de inicialização estático 2");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático");
+    }
     public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
-        for (int episodio: this.episodios) {
-            System.out.println(episodio + " ");
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
         }
         System.out.println();
     }
@@ -42,7 +51,7 @@ public class Anime {
     }
 
     public void setEpisodios(int[] episodios) {
-        this.episodios = episodios;
+        Anime.episodios = episodios;
     }
 }
 
