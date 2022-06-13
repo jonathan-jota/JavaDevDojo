cdc068f06a9c027164e3cd50069ee11eff8f5ded
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-06-13 16:45:24 -0300

    Polimorfismo pt01 - Introdução

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadorImposto.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+
+public class CalculadorImposto {
+    public static void calculaImpostoComputador(Computador computador) {
+        System.out.println("Relatorio de imposto do computador");
+        double imposto = computador.calcularImposto();
+        System.out.println("Computador " + computador.getNome());
+        System.out.println("Valor " + computador.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
+    }
+
+    public static void calculaImpostoTomate(Tomate tomate) {
+        System.out.println("Relatorio de imposto do tomate");
+        double imposto = tomate.calcularImposto();
+        System.out.println("Computador " + tomate.getNome());
+        System.out.println("Valor " + tomate.getValor());
+        System.out.println("Imposto a ser pago " + imposto);
+    }
+}
