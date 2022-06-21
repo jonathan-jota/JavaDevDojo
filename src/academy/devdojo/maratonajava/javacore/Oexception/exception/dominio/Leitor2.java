318b14afecb6ee05a03be7532785ee3b89c75235
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-06-20 22:36:34 -0300

    Exceções pt09 - Try with Resources

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/dominio/Leitor2.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor2 implements Closeable {
+    @Override
+    public void close() throws IOException {
+        System.out.println("Fechando Leitor 2");
+    }
+}
+
+
