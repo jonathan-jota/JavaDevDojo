f3d73d8634903f12691565f95c185f7aefc96d88
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-06-24 08:57:24 -0300

    Classes utilitárias - Internacionalização Números com Locale

---
+++ src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getInstance();
+        nfa[1] = NumberFormat.getInstance(localeJP);
+        nfa[2] = NumberFormat.getInstance(localeBR);
+        nfa[3] = NumberFormat.getInstance(localeIT);
+        double valor = 10_000_000.1234;
+        for (NumberFormat numberFormat:
+             nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+    }
+}
