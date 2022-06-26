60fd95e384f30c811c26af3ad4f36bb9c964c45d
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-06-26 00:37:51 -0300

    Classes utilitárias - ChronoUnit

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
@@ -21,5 +22,6 @@ public class PeriodTest01 {
         System.out.println(p5);
         System.out.println(p3.getMonths());
         System.out.println(Period.between(LocalDate.now(), LocalDate.now().plusDays(p3.getDays())).getMonths());
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.DAYS));
     }
 }
