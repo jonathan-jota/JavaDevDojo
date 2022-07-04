f4bc2c62891cd62563d85be7a2042c0b1300397f
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-07-04 08:36:36 -0300

    Classes Utilitárias - Serialization pt 02

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
@@ -1,16 +1,43 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.dominio;
 
-import java.io.Serializable;
+import java.io.*;
 
 public class Aluno implements Serializable {
+
+    private static final long serialVersionUID = 739959230178355185L;
     private Long id;
     private String nome;
-    private String password;
+    private transient String password;
+
+    private static final String NOME_ESCOLA = "DevDojo";
+
+    private transient Turma turma;
 
     public Aluno(Long id, String nome, String password) {
         this.id = id;
         this.nome = nome;
         this.password = password;
+    }
+
+
+    private void writeObject(ObjectOutputStream oos) {
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -19,7 +46,17 @@ public class Aluno implements Serializable {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
+                ", NOME_ESCOLA='" + NOME_ESCOLA + '\'' +
+                ", turma='" + turma + '\'' +
                 '}';
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
     }
 
     public Long getId() {
--- src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoeTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.test;
 
 import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Turma;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -12,7 +13,9 @@ import java.nio.file.Paths;
 public class SerializacaoeTest01 {
     public static void main(String[] args) {
         Aluno aluno = new Aluno(1L, "Jose", "123");
-        //serializar(aluno);
+        Turma turma = new Turma("Maratona Java");
+        aluno.setTurma(turma);
+        serializar(aluno);
         deserializar();
     }
 
